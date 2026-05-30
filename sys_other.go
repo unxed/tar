b75d10d478560b8a2c9c78191f48650ea9e3dfe5
Ivan Sorokin <unxed@mail.ru>
2026-05-30 15:34:45 +0200

    Fix build

--- sys_other.go
+++ sys_other.go
@@ -15,17 +15,6 @@ func sysHeader(fi os.FileInfo, hdr *tar.Header) {
 func extractSpecialFile(path string, hdr *tar.Header) error {
 	return nil
 }
-func getFileSecurity(path string) ([]byte, error) {
-	return nil, nil
-}
-
-func applyNtfsAcl(path string, acl []byte) error {
-	return nil
-}
-
-func getAlternativeDataStreams(path string) ([]string, error) {
-	return nil, nil
-}
 
 func sysXattrs(path string, hdr *tar.Header) error {
 	return nil
