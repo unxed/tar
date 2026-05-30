805b15c7af6bc8e3376e915cd1e7ea4158a9c6a7
Ivan Sorokin <unxed@mail.ru>
2026-05-30 11:51:40 +0200

    Fix build

--- sys_other.go
+++ sys_other.go
@@ -14,7 +14,9 @@ func sysHeader(fi os.FileInfo, hdr *tar.Header) {
 
 func extractSpecialFile(path string, hdr *tar.Header) error {
 	return nil
-}func sysXattrs(path string, hdr *tar.Header) error {
+}
+
+func sysXattrs(path string, hdr *tar.Header) error {
 	return nil
 }
 
