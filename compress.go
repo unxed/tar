24edca660035d8032cc9e0d3580ddde466a7fccd
Ivan Sorokin <unxed@mail.ru>
2026-06-28 13:33:02 +0200

    Switching the ZSTD decoder to dynamic parallelism (GOMAXPROCS / 2) for optimal scaling.

--- compress.go
+++ compress.go
@@ -490,7 +490,11 @@ func newTarZstdReader(r io.Reader) (io.ReadCloser, error) {
 	dec, _ := zstdDecoderPool.Get().(*zstd.Decoder)
 	if dec == nil {
 		var err error
-		dec, err = zstd.NewReader(nil, zstd.WithDecoderConcurrency(1), zstd.WithDecoderMaxWindow(512<<20), zstd.WithDecoderMaxMemory(512<<20))
+		concurrency := runtime.GOMAXPROCS(0) / 2
+		if concurrency < 1 {
+			concurrency = 1
+		}
+		dec, err = zstd.NewReader(nil, zstd.WithDecoderConcurrency(concurrency), zstd.WithDecoderMaxWindow(512<<20), zstd.WithDecoderMaxMemory(512<<20))
 		if err != nil {
 			return nil, err
 		}
